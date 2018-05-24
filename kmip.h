5d38dc77964df3b259cbcc48f86ca244099909cf
Peter Hamilton <peter.allen.hamilton@gmail.com>
2018-05-24 14:42:25 -0400

    Add encoding support for attributes
    This change adds broad encoding support for various attributes,
    including names, unique IDs, and various cryptographic values.
    Various enums have been added to support this and the unit test
    suite has been upgraded to test the new encoding functions. The
    test suite will also now display the expected/observed encodings
    when an encoding error is detected.

--- kmip.h
+++ kmip.h
@@ -41,31 +41,12 @@ typedef double real64;
 #define KMIP_TRUE 1
 #define KMIP_FALSE 0
 
-#define KMIP_OK 0
-#define KMIP_NOT_IMPLEMENTED -1
-#define KMIP_ERROR_BUFFER_FULL -2
+#define KMIP_UNSET -1
 
-struct protocol_version
-{
-    int32 major;
-    int32 minor;
-};
-
-struct error_frame
-{
-    char function[100];
-    int line;
-};
-
-struct kmip
-{
-    uint8 *buffer;
-    uint8 *index;
-    size_t size;
-    enum kmip_version version;
-    struct error_frame errors[20];
-    struct error_frame *frame_index;
-};
+#define KMIP_OK                      0
+#define KMIP_NOT_IMPLEMENTED        -1
+#define KMIP_ERROR_BUFFER_FULL      -2
+#define KMIP_ERROR_ATTR_UNSUPPORTED -3
 
 #define ARRAY_LENGTH(A) (sizeof((A)) / sizeof((A)[0]))
 #define CHECK_BUFFER_FULL(A, B)                         \
@@ -86,6 +67,56 @@ do                                                      \
         return((B));                                    \
     }                                                   \
 } while(0)
+#define TAG_TYPE(A, B) (((A) << 8) | (uint8)(B))
+
+struct error_frame
+{
+    char function[100];
+    int line;
+};
+
+struct kmip
+{
+    uint8 *buffer;
+    uint8 *index;
+    size_t size;
+    enum kmip_version version;
+    struct error_frame errors[20];
+    struct error_frame *frame_index;
+};
+
+struct template_attribute
+{
+    struct name *name;
+    struct attribute *attributes;
+    size_t attribute_count;
+};
+
+struct attribute
+{
+    enum attribute_type type;
+    int32 index;
+    void *value;
+};
+
+struct name
+{
+    char *value;
+    size_t size;
+    enum name_type type;
+};
+
+struct text_string
+{
+    char *value;
+    size_t size;
+};
+
+struct protocol_version
+{
+    int32 major;
+    int32 minor;
+};
 
 void
 kmip_clear_errors(struct kmip *ctx)
@@ -190,7 +221,7 @@ encode_integer(struct kmip *ctx, enum tag t, int32 value)
 {
     CHECK_BUFFER_FULL(ctx, 16);
     
-    encode_int32_be(ctx, (t << 8) | (uint8)KMIP_TYPE_INTEGER);
+    encode_int32_be(ctx, TAG_TYPE(t, KMIP_TYPE_INTEGER));
     encode_int32_be(ctx, 4);
     encode_int32_be(ctx, value);
     encode_int32_be(ctx, 0);
@@ -203,7 +234,7 @@ encode_long(struct kmip *ctx, enum tag t, int64 value)
 {
     CHECK_BUFFER_FULL(ctx, 16);
     
-    encode_int32_be(ctx, (t << 8) | (uint8)KMIP_TYPE_LONG_INTEGER);
+    encode_int32_be(ctx, TAG_TYPE(t, KMIP_TYPE_LONG_INTEGER));
     encode_int32_be(ctx, 8);
     encode_int64_be(ctx, value);
     
@@ -223,7 +254,7 @@ encode_enum(struct kmip *ctx, enum tag t, int32 value)
 {
     CHECK_BUFFER_FULL(ctx, 16);
     
-    encode_int32_be(ctx, (t << 8) | (uint8)KMIP_TYPE_ENUMERATION);
+    encode_int32_be(ctx, TAG_TYPE(t, KMIP_TYPE_ENUMERATION));
     encode_int32_be(ctx, 4);
     encode_int32_be(ctx, value);
     encode_int32_be(ctx, 0);
@@ -236,7 +267,7 @@ encode_bool(struct kmip *ctx, enum tag t, int32 value)
 {
     CHECK_BUFFER_FULL(ctx, 16);
     
-    encode_int32_be(ctx, (t << 8) | (uint8)KMIP_TYPE_BOOLEAN);
+    encode_int32_be(ctx, TAG_TYPE(t, KMIP_TYPE_BOOLEAN));
     encode_int32_be(ctx, 8);
     encode_int32_be(ctx, 0);
     encode_int32_be(ctx, value);
@@ -247,10 +278,10 @@ encode_bool(struct kmip *ctx, enum tag t, int32 value)
 int
 encode_text_string(struct kmip *ctx, enum tag t, const char *value, uint32 length)
 {
-    uint8 padding = 8 - (length % 8);
+    uint8 padding = (8 - (length % 8)) % 8;
     CHECK_BUFFER_FULL(ctx, 8 + length + padding);
     
-    encode_int32_be(ctx, (t << 8) | (uint8)KMIP_TYPE_TEXT_STRING);
+    encode_int32_be(ctx, TAG_TYPE(t, KMIP_TYPE_TEXT_STRING));
     encode_int32_be(ctx, length);
     
     for(uint32 i = 0; i < length; i++)
@@ -271,7 +302,7 @@ encode_byte_string(struct kmip *ctx, enum tag t, const int8 *value, uint32 lengt
     uint8 padding = 8 - (length % 8);
     CHECK_BUFFER_FULL(ctx, 8 + length + padding);
     
-    encode_int32_be(ctx, (t << 8) | (uint8)KMIP_TYPE_BYTE_STRING);
+    encode_int32_be(ctx, TAG_TYPE(t, KMIP_TYPE_BYTE_STRING));
     encode_int32_be(ctx, length);
     
     for(uint32 i = 0; i < length; i++)
@@ -291,7 +322,7 @@ encode_date_time(struct kmip *ctx, enum tag t, uint64 value)
 {
     CHECK_BUFFER_FULL(ctx, 16);
     
-    encode_int32_be(ctx, (t << 8) | (uint8)KMIP_TYPE_DATE_TIME);
+    encode_int32_be(ctx, TAG_TYPE(t, KMIP_TYPE_DATE_TIME));
     encode_int32_be(ctx, 8);
     encode_int64_be(ctx, value);
     
@@ -303,7 +334,7 @@ encode_interval(struct kmip *ctx, enum tag t, uint32 value)
 {
     CHECK_BUFFER_FULL(ctx, 16);
     
-    encode_int32_be(ctx, (t << 8) | (uint8)KMIP_TYPE_INTERVAL);
+    encode_int32_be(ctx, TAG_TYPE(t, KMIP_TYPE_INTERVAL));
     encode_int32_be(ctx, 4);
     encode_int32_be(ctx, value);
     encode_int32_be(ctx, 0);
@@ -311,14 +342,197 @@ encode_interval(struct kmip *ctx, enum tag t, uint32 value)
     return(KMIP_OK);
 }
 
+/*
 int
-encode_protocol_version(struct kmip *ctx, const struct protocol_version *pv)
+encode_template_attribute(struct kmip *ctx, struct template_attribute *t)
+{
+};
+*/
+
+int
+encode_name(struct kmip *ctx, struct name *n)
+{
+    /* TODO (peter-hamilton) Check for n == NULL? */
+    
+    int result = 0;
+    
+    result = encode_int32_be(
+        ctx, TAG_TYPE(KMIP_TAG_NAME, KMIP_TYPE_STRUCTURE));
+    CHECK_RESULT(ctx, result);
+    
+    uint8 *length_index = ctx->index;
+    uint8 *value_index = ctx->index += 4;
+    
+    result = encode_text_string(ctx, KMIP_TAG_NAME_VALUE, n->value, n->size);
+    CHECK_RESULT(ctx, result);
+    result = encode_enum(ctx, KMIP_TAG_NAME_TYPE, n->type);
+    CHECK_RESULT(ctx, result);
+    
+    uint8 *curr_index = ctx->index;
+    ctx->index = length_index;
+    
+    result = encode_int32_be(ctx, curr_index - value_index);
+    CHECK_RESULT(ctx, result);
+    
+    ctx->index = curr_index;
+    
+    return(KMIP_OK);
+}
+
+int
+encode_attribute_name(struct kmip *ctx, enum attribute_type type)
+{
+    int result = 0;
+    enum tag t = KMIP_TAG_ATTRIBUTE_NAME;
+    
+    switch(type)
+    {
+        case KMIP_ATTR_UNIQUE_IDENTIFIER:
+        result = encode_text_string(ctx, t, "Unique Identifier", 17);
+        break;
+        
+        case KMIP_ATTR_NAME:
+        result = encode_text_string(ctx, t, "Name", 4);
+        break;
+        
+        case KMIP_ATTR_OBJECT_TYPE:
+        result = encode_text_string(ctx, t, "Object Type", 11);
+        break;
+        
+        case KMIP_ATTR_CRYPTOGRAPHIC_ALGORITHM:
+        result = encode_text_string(ctx, t, "Cryptographic Algorithm", 23);
+        break;
+        
+        case KMIP_ATTR_CRYPTOGRAPHIC_LENGTH:
+        result = encode_text_string(ctx, t, "Cryptographic Length", 20);
+        break;
+        
+        case KMIP_ATTR_OPERATION_POLICY_NAME:
+        result = encode_text_string(ctx, t, "Operation Policy Name", 21);
+        break;
+        
+        case KMIP_ATTR_CRYPTOGRAPHIC_USAGE_MASK:
+        result = encode_text_string(ctx, t, "Cryptographic Usage Mask", 24);
+        break;
+        
+        case KMIP_ATTR_STATE:
+        result = encode_text_string(ctx, t, "State", 5);
+        break;
+        
+        default:
+        kmip_push_error_frame(ctx, __func__, __LINE__);
+        return(KMIP_ERROR_ATTR_UNSUPPORTED);
+        break;
+    };
+    
+    CHECK_RESULT(ctx, result);
+    
+    return(KMIP_OK);
+}
+
+int
+encode_attribute(struct kmip *ctx, struct attribute *attr)
+{
+    /* TODO (peter-hamilton) Check attr == NULL? */
+    /* TODO (peter-hamilton) Cehck attr->value == NULL? */
+    
+    int result = 0;
+    result = encode_int32_be(
+        ctx, 
+        TAG_TYPE(KMIP_TAG_ATTRIBUTE, KMIP_TYPE_STRUCTURE));
+    CHECK_RESULT(ctx, result);
+    
+    uint8 *length_index = ctx->index;
+    uint8 *value_index = ctx->index += 4;
+    
+    if(attr->index != KMIP_UNSET)
+    {
+        result = encode_integer(ctx, KMIP_TAG_ATTRIBUTE_INDEX, attr->index);
+        CHECK_RESULT(ctx, result);
+    }
+    
+    result = encode_attribute_name(ctx, attr->type);
+    CHECK_RESULT(ctx, result);
+    
+    uint8 *curr_index = ctx->index;
+    uint8 *tag_index = ctx->index;
+    enum tag t = KMIP_TAG_ATTRIBUTE_VALUE;
+    
+    switch(attr->type)
+    {
+        case KMIP_ATTR_UNIQUE_IDENTIFIER:
+        result = encode_text_string(
+            ctx, t, 
+            ((struct text_string*)attr->value)->value,
+            ((struct text_string*)attr->value)->size);
+        break;
+        
+        case KMIP_ATTR_NAME:
+        /* TODO (peter-hamilton) This is messy. Clean it up? */
+        result = encode_name(ctx, (struct name*)attr->value);
+        CHECK_RESULT(ctx, result);
+        curr_index = ctx->index;
+        ctx->index = tag_index;
+        result = encode_int32_be(
+            ctx,
+            TAG_TYPE(KMIP_TAG_ATTRIBUTE_VALUE, KMIP_TYPE_STRUCTURE));
+        ctx->index = curr_index;
+        break;
+        
+        case KMIP_ATTR_OBJECT_TYPE:
+        result = encode_enum(ctx, t, *(int32 *)attr->value);
+        break;
+        
+        case KMIP_ATTR_CRYPTOGRAPHIC_ALGORITHM:
+        result = encode_enum(ctx, t, *(int32 *)attr->value);
+        break;
+        
+        case KMIP_ATTR_CRYPTOGRAPHIC_LENGTH:
+        result = encode_integer(ctx, t, *(int32 *)attr->value);
+        break;
+        
+        case KMIP_ATTR_OPERATION_POLICY_NAME:
+        result = encode_text_string(
+            ctx, t, 
+            ((struct text_string*)attr->value)->value,
+            ((struct text_string*)attr->value)->size);
+        break;
+        
+        case KMIP_ATTR_CRYPTOGRAPHIC_USAGE_MASK:
+        result = encode_integer(ctx, t, *(int32 *)attr->value);
+        break;
+        
+        case KMIP_ATTR_STATE:
+        result = encode_enum(ctx, t, *(int32 *)attr->value);
+        break;
+        
+        default:
+        kmip_push_error_frame(ctx, __func__, __LINE__);
+        return(KMIP_ERROR_ATTR_UNSUPPORTED);
+        break;
+    };
+    CHECK_RESULT(ctx, result);
+    
+    curr_index = ctx->index;
+    ctx->index = length_index;
+    
+    result = encode_int32_be(ctx, curr_index - value_index);
+    CHECK_RESULT(ctx, result);
+    
+    ctx->index = curr_index;
+    
+    return(KMIP_OK);
+}
+
+int
+encode_protocol_version(struct kmip *ctx, 
+                        const struct protocol_version *pv)
 {
     CHECK_BUFFER_FULL(ctx, 40);
     
     encode_int32_be(
         ctx, 
-        (KMIP_TAG_PROTOCOL_VERSION << 8) | (uint8)KMIP_TYPE_STRUCTURE);
+        TAG_TYPE(KMIP_TAG_PROTOCOL_VERSION, KMIP_TYPE_STRUCTURE));
     
     uint8 *length_index = ctx->index;
     uint8 *value_index = ctx->index += 4;
--- test.c
+++ test.c
@@ -42,12 +42,15 @@ report_test_result(struct kmip *ctx, const uint8 *expected, const uint8 *observe
     {
         for(size_t i = 0; i < ctx->size; i++)
         {
-            /* printf("%zu: %o - %o\n", i, expected[i], observed[i]); */
             if(expected[i] != observed[i])
             {
                 printf("FAIL - %s\n", function);
                 printf("- byte mismatch at: %zu (exp: %o, obs: %o)\n",
                        i, expected[i], observed[i]);
+                for(size_t j = 0; j < ctx->size; j++)
+                {
+                    printf("- %zu: %o - %o\n", j, expected[j], observed[j]);
+                }
                 return(1);
             }
         }
@@ -75,9 +78,7 @@ test_encode_integer(void)
     
     uint8 observed[16] = {0};
     struct kmip ctx = {0};
-    ctx.buffer = observed;
-    ctx.index = observed;
-    ctx.size = ARRAY_LENGTH(observed);
+    kmip_init(&ctx, observed, ARRAY_LENGTH(observed), KMIP_1_0);
     
     int result = encode_integer(&ctx, KMIP_TAG_DEFAULT, 8);
     return(report_test_result(&ctx, expected, observed, result, __func__));
@@ -93,9 +94,7 @@ test_encode_long(void)
     
     uint8 observed[16] = {0};
     struct kmip ctx = {0};
-    ctx.buffer = observed;
-    ctx.index = observed;
-    ctx.size = ARRAY_LENGTH(observed);
+    kmip_init(&ctx, observed, ARRAY_LENGTH(observed), KMIP_1_0);
     
     int result = encode_long(&ctx, KMIP_TAG_DEFAULT, 123456789000000000);
     return(report_test_result(&ctx, expected, observed, result, __func__));
@@ -112,9 +111,7 @@ test_encode_text_string(void)
     
     uint8 observed[24] = {0};
     struct kmip ctx = {0};
-    ctx.buffer = observed;
-    ctx.index = observed;
-    ctx.size = ARRAY_LENGTH(observed);
+    kmip_init(&ctx, observed, ARRAY_LENGTH(observed), KMIP_1_0);
     
     int result = encode_text_string(&ctx, KMIP_TAG_DEFAULT, "Hello World", 11);
     return(report_test_result(&ctx, expected, observed, result, __func__));
@@ -130,9 +127,7 @@ test_encode_byte_string(void)
     
     uint8 observed[16] = {0};
     struct kmip ctx = {0};
-    ctx.buffer = observed;
-    ctx.index = observed;
-    ctx.size = ARRAY_LENGTH(observed);
+    kmip_init(&ctx, observed, ARRAY_LENGTH(observed), KMIP_1_0);
     int8 str[3] = {0x01, 0x02, 0x03};
     
     int result = encode_byte_string(&ctx, KMIP_TAG_DEFAULT, str, 3);
@@ -149,9 +144,7 @@ test_encode_date_time(void)
     
     uint8 observed[16] = {0};
     struct kmip ctx = {0};
-    ctx.buffer = observed;
-    ctx.index = observed;
-    ctx.size = ARRAY_LENGTH(observed);
+    kmip_init(&ctx, observed, ARRAY_LENGTH(observed), KMIP_1_0);
     
     int result = encode_date_time(&ctx, KMIP_TAG_DEFAULT, 1205495800);
     return(report_test_result(&ctx, expected, observed, result, __func__));
@@ -167,11 +160,263 @@ test_encode_interval(void)
     
     uint8 observed[16] = {0};
     struct kmip ctx = {0};
-    ctx.buffer = observed;
-    ctx.index = observed;
-    ctx.size = ARRAY_LENGTH(observed);
+    kmip_init(&ctx, observed, ARRAY_LENGTH(observed), KMIP_1_0);
     
     int result = encode_interval(&ctx, KMIP_TAG_DEFAULT, 864000);
+    return(report_test_result(&ctx, expected, observed, result, __func__));
+}
+
+int
+test_encode_name(void)
+{
+    uint8 expected[48] = {
+        0x42, 0x00, 0x53, 0x01, 0x00, 0x00, 0x00, 0x28,
+        0x42, 0x00, 0x55, 0x07, 0x00, 0x00, 0x00, 0x09,
+        0x54, 0x65, 0x6D, 0x70, 0x6C, 0x61, 0x74, 0x65,
+        0x31, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x42, 0x00, 0x54, 0x05, 0x00, 0x00, 0x00, 0x04,
+        0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00
+    };
+    
+    uint8 observed[48] = {0};
+    struct kmip ctx = {0};
+    kmip_init(&ctx, observed, ARRAY_LENGTH(observed), KMIP_1_0);
+    
+    char *value = "Template1";
+    struct name n = {0};
+    n.value = value;
+    n.size = 9;
+    n.type = KMIP_NAME_UNINTERPRETED_TEXT_STRING;
+    
+    int result = encode_name(&ctx, &n);
+    return(report_test_result(&ctx, expected, observed, result, __func__));
+}
+
+int
+test_encode_attribute_unique_identifier(void)
+{
+    uint8 expected[88] = {
+        0x42, 0x00, 0x08, 0x01, 0x00, 0x00, 0x00, 0x50,
+        0x42, 0x00, 0x0A, 0x07, 0x00, 0x00, 0x00, 0x11,
+        0x55, 0x6E, 0x69, 0x71, 0x75, 0x65, 0x20, 0x49,
+        0x64, 0x65, 0x6E, 0x74, 0x69, 0x66, 0x69, 0x65,
+        0x72, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x42, 0x00, 0x0B, 0x07, 0x00, 0x00, 0x00, 0x24,
+        0x34, 0x39, 0x61, 0x31, 0x63, 0x61, 0x38, 0x38,
+        0x2D, 0x36, 0x62, 0x65, 0x61, 0x2D, 0x34, 0x66,
+        0x62, 0x32, 0x2D, 0x62, 0x34, 0x35, 0x30, 0x2D,
+        0x37, 0x65, 0x35, 0x38, 0x38, 0x30, 0x32, 0x63,
+        0x33, 0x30, 0x33, 0x38, 0x00, 0x00, 0x00, 0x00
+    };
+    
+    uint8 observed[88] = {0};
+    struct kmip ctx = {0};
+    kmip_init(&ctx, observed, ARRAY_LENGTH(observed), KMIP_1_0);
+    
+    struct text_string uuid = {0};
+    uuid.value = "49a1ca88-6bea-4fb2-b450-7e58802c3038";
+    uuid.size = 36;
+    struct attribute attr = {0};
+    attr.type = KMIP_ATTR_UNIQUE_IDENTIFIER;
+    attr.index = KMIP_UNSET;
+    attr.value = &uuid;
+    
+    int result = encode_attribute(&ctx, &attr);
+    return(report_test_result(&ctx, expected, observed, result, __func__));
+}
+
+int
+test_encode_attribute_name(void)
+{
+    uint8 expected[72] = {
+        0x42, 0x00, 0x08, 0x01, 0x00, 0x00, 0x00, 0x40,
+        0x42, 0x00, 0x0A, 0x07, 0x00, 0x00, 0x00, 0x04,
+        0x4E, 0x61, 0x6D, 0x65, 0x00, 0x00, 0x00, 0x00,
+        0x42, 0x00, 0x0B, 0x01, 0x00, 0x00, 0x00, 0x28,
+        0x42, 0x00, 0x55, 0x07, 0x00, 0x00, 0x00, 0x09,
+        0x54, 0x65, 0x6D, 0x70, 0x6C, 0x61, 0x74, 0x65,
+        0x31, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x42, 0x00, 0x54, 0x05, 0x00, 0x00, 0x00, 0x04,
+        0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00
+    };
+    
+    uint8 observed[72] = {0};
+    struct kmip ctx = {0};
+    kmip_init(&ctx, observed, ARRAY_LENGTH(observed), KMIP_1_0);
+    
+    char *value = "Template1";
+    struct name n = {0};
+    n.value = value;
+    n.size = 9;
+    n.type = KMIP_NAME_UNINTERPRETED_TEXT_STRING;
+    
+    struct attribute attr = {0};
+    attr.type = KMIP_ATTR_NAME;
+    attr.index = KMIP_UNSET;
+    attr.value = &n;
+    
+    int result = encode_attribute(&ctx, &attr);
+    return(report_test_result(&ctx, expected, observed, result, __func__));
+}
+
+int
+test_encode_attribute_object_type(void)
+{
+    uint8 expected[48] = {
+        0x42, 0x00, 0x08, 0x01, 0x00, 0x00, 0x00, 0x28,
+        0x42, 0x00, 0x0A, 0x07, 0x00, 0x00, 0x00, 0x0B,
+        0x4F, 0x62, 0x6A, 0x65, 0x63, 0x74, 0x20, 0x54,
+        0x79, 0x70, 0x65, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x42, 0x00, 0x0B, 0x05, 0x00, 0x00, 0x00, 0x04,
+        0x00, 0x00, 0x00, 0x02, 0x00, 0x00, 0x00, 0x00
+    };
+    
+    uint8 observed[48] = {0};
+    struct kmip ctx = {0};
+    kmip_init(&ctx, observed, ARRAY_LENGTH(observed), KMIP_1_0);
+    
+    enum object_type t = KMIP_OBJTYPE_SYMMETRIC_KEY;
+    struct attribute attr = {0};
+    attr.type = KMIP_ATTR_OBJECT_TYPE;
+    attr.index = KMIP_UNSET;
+    attr.value = &t;
+    
+    int result = encode_attribute(&ctx, &attr);
+    return(report_test_result(&ctx, expected, observed, result, __func__));
+}
+
+int
+test_encode_attribute_cryptographic_algorithm(void)
+{
+    uint8 expected[56] = {
+        0x42, 0x00, 0x08, 0x01, 0x00, 0x00, 0x00, 0x30,
+        0x42, 0x00, 0x0A, 0x07, 0x00, 0x00, 0x00, 0x17,
+        0x43, 0x72, 0x79, 0x70, 0x74, 0x6F, 0x67, 0x72,
+        0x61, 0x70, 0x68, 0x69, 0x63, 0x20, 0x41, 0x6C,
+        0x67, 0x6F, 0x72, 0x69, 0x74, 0x68, 0x6D, 0x00,
+        0x42, 0x00, 0x0B, 0x05, 0x00, 0x00, 0x00, 0x04,
+        0x00, 0x00, 0x00, 0x03, 0x00, 0x00, 0x00, 0x00
+    };
+    
+    uint8 observed[56] = {0};
+    struct kmip ctx = {0};
+    kmip_init(&ctx, observed, ARRAY_LENGTH(observed), KMIP_1_0);
+    
+    enum cryptographic_algorithm a = KMIP_CRYPTOALG_AES;
+    struct attribute attr = {0};
+    attr.type = KMIP_ATTR_CRYPTOGRAPHIC_ALGORITHM;
+    attr.index = KMIP_UNSET;
+    attr.value = &a;
+    
+    int result = encode_attribute(&ctx, &attr);
+    return(report_test_result(&ctx, expected, observed, result, __func__));
+}
+
+int
+test_encode_attribute_cryptographic_length(void)
+{
+    uint8 expected[56] = {
+        0x42, 0x00, 0x08, 0x01, 0x00, 0x00, 0x00, 0x30,
+        0x42, 0x00, 0x0A, 0x07, 0x00, 0x00, 0x00, 0x14,
+        0x43, 0x72, 0x79, 0x70, 0x74, 0x6F, 0x67, 0x72,
+        0x61, 0x70, 0x68, 0x69, 0x63, 0x20, 0x4C, 0x65,
+        0x6E, 0x67, 0x74, 0x68, 0x00, 0x00, 0x00, 0x00,
+        0x42, 0x00, 0x0B, 0x02, 0x00, 0x00, 0x00, 0x04,
+        0x00, 0x00, 0x00, 0x80, 0x00, 0x00, 0x00, 0x00
+    };
+    
+    uint8 observed[56] = {0};
+    struct kmip ctx = {0};
+    kmip_init(&ctx, observed, ARRAY_LENGTH(observed), KMIP_1_0);
+    
+    int32 length = 128;
+    struct attribute attr = {0};
+    attr.type = KMIP_ATTR_CRYPTOGRAPHIC_LENGTH;
+    attr.index = KMIP_UNSET;
+    attr.value = &length;
+    
+    int result = encode_attribute(&ctx, &attr);
+    return(report_test_result(&ctx, expected, observed, result, __func__));
+}
+
+int
+test_encode_attribute_operation_policy_name(void)
+{
+    uint8 expected[56] = {
+        0x42, 0x00, 0x08, 0x01, 0x00, 0x00, 0x00, 0x30,
+        0x42, 0x00, 0x0A, 0x07, 0x00, 0x00, 0x00, 0x15,
+        0x4F, 0x70, 0x65, 0x72, 0x61, 0x74, 0x69, 0x6F,
+        0x6E, 0x20, 0x50, 0x6F, 0x6C, 0x69, 0x63, 0x79,
+        0x20, 0x4E, 0x61, 0x6D, 0x65, 0x00, 0x00, 0x00,
+        0x42, 0x00, 0x0B, 0x07, 0x00, 0x00, 0x00, 0x07,
+        0x64, 0x65, 0x66, 0x61, 0x75, 0x6C, 0x74, 0x00
+    };
+    
+    uint8 observed[56] = {0};
+    struct kmip ctx = {0};
+    kmip_init(&ctx, observed, ARRAY_LENGTH(observed), KMIP_1_0);
+    
+    struct text_string policy = {0};
+    policy.value = "default";
+    policy.size = 7;
+    struct attribute attr = {0};
+    attr.type = KMIP_ATTR_OPERATION_POLICY_NAME;
+    attr.index = KMIP_UNSET;
+    attr.value = &policy;
+    
+    int result = encode_attribute(&ctx, &attr);
+    return(report_test_result(&ctx, expected, observed, result, __func__));
+}
+
+int
+test_encode_attribute_cryptographic_usage_mask(void)
+{
+    uint8 expected[56] = {
+        0x42, 0x00, 0x08, 0x01, 0x00, 0x00, 0x00, 0x30,
+        0x42, 0x00, 0x0A, 0x07, 0x00, 0x00, 0x00, 0x18,
+        0x43, 0x72, 0x79, 0x70, 0x74, 0x6F, 0x67, 0x72,
+        0x61, 0x70, 0x68, 0x69, 0x63, 0x20, 0x55, 0x73,
+        0x61, 0x67, 0x65, 0x20, 0x4D, 0x61, 0x73, 0x6B,
+        0x42, 0x00, 0x0B, 0x02, 0x00, 0x00, 0x00, 0x04,
+        0x00, 0x00, 0x00, 0x0C, 0x00, 0x00, 0x00, 0x00
+    };
+    
+    uint8 observed[56] = {0};
+    struct kmip ctx = {0};
+    kmip_init(&ctx, observed, ARRAY_LENGTH(observed), KMIP_1_0);
+    
+    int32 mask = KMIP_CRYPTOMASK_ENCRYPT | KMIP_CRYPTOMASK_DECRYPT;
+    struct attribute attr = {0};
+    attr.type = KMIP_ATTR_CRYPTOGRAPHIC_USAGE_MASK;
+    attr.index = KMIP_UNSET;
+    attr.value = &mask;
+    
+    int result = encode_attribute(&ctx, &attr);
+    return(report_test_result(&ctx, expected, observed, result, __func__));
+}
+
+int
+test_encode_attribute_state(void)
+{
+    uint8 expected[40] = {
+        0x42, 0x00, 0x08, 0x01, 0x00, 0x00, 0x00, 0x20,
+        0x42, 0x00, 0x0A, 0x07, 0x00, 0x00, 0x00, 0x05, 
+        0x53, 0x74, 0x61, 0x74, 0x65, 0x00, 0x00, 0x00,
+        0x42, 0x00, 0x0B, 0x05, 0x00, 0x00, 0x00, 0x04,
+        0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00
+    };
+    
+    uint8 observed[40] = {0};
+    struct kmip ctx = {0};
+    kmip_init(&ctx, observed, ARRAY_LENGTH(observed), KMIP_1_0);
+    
+    enum state s = KMIP_STATE_PRE_ACTIVE;
+    struct attribute attr = {0};
+    attr.type = KMIP_ATTR_STATE;
+    attr.index = KMIP_UNSET;
+    attr.value = &s;
+    
+    int result = encode_attribute(&ctx, &attr);
     return(report_test_result(&ctx, expected, observed, result, __func__));
 }
 
@@ -240,7 +485,7 @@ test_buffer_full_and_resize(void)
 int
 main(void)
 {
-    int num_tests = 8;
+    int num_tests = 17;
     int num_failures = 0;
     
     printf("Tests\n");
@@ -253,6 +498,15 @@ main(void)
     num_failures += test_encode_byte_string();
     num_failures += test_encode_date_time();
     num_failures += test_encode_interval();
+    num_failures += test_encode_name();
+    num_failures += test_encode_attribute_unique_identifier();
+    num_failures += test_encode_attribute_name();
+    num_failures += test_encode_attribute_object_type();
+    num_failures += test_encode_attribute_cryptographic_algorithm();
+    num_failures += test_encode_attribute_cryptographic_length();
+    num_failures += test_encode_attribute_operation_policy_name();
+    num_failures += test_encode_attribute_cryptographic_usage_mask();
+    num_failures += test_encode_attribute_state();
     num_failures += test_encode_protocol_version();
     
     printf("\nSummary\n");
